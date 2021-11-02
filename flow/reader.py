f7ac5548d1a2f96ef527404fee93c4620eb6052e
crvernon <chrisrvernon@gmail.com>
2021-11-02 14:58:29 -0400

    initial data reader functions

---
+++ flow/reader.py
@@ -0,0 +1,28 @@
+import pkg_resources
+
+import pandas as pd
+
+
+def get_water_use_2015():
+    """asdf"""
+
+    data = pkg_resources.resource_filename('flow', 'data/usco2015v2.0.xlsx')
+
+    return pd.read_excel(data, skiprows=1, dtype={'FIPS': str})
+
+
+def get_interconnect_data():
+    """asdf"""
+
+    data = pkg_resources.resource_filename('flow', 'data/county_interconnect_list.xlsx')
+
+    # read in county-interconnect crosswalk
+    return pd.read_excel(data, dtype={'FIPS': str}, usecols=["FIPS", "Interconnect"])
+
+
+def get_water_use_1995():
+    """asdf"""
+
+    data = pkg_resources.resource_filename('flow', 'data/usco1995.xlsx')
+
+    return pd.read_excel(data,  dtype={'StateCode': str, 'CountyCode': str})
