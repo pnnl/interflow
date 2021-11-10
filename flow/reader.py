d2be95dde13aa390830458495f7c103c26c19007
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2021-11-09 16:49:24 -0800

    Add docstrings to reader.py

--- flow/reader.py
+++ flow/reader.py
@@ -6,7 +6,7 @@ import pandas as pd
 def get_water_use_2015():
     """Read in a dataframe of 2015 USGS Water Use Data.
 
-        :return:                        dataframe of values
+        :return:                        Dataframe of a number of water values for 2015 at the county level
 
         """
 
@@ -16,7 +16,11 @@ def get_water_use_2015():
 
 
 def get_interconnect_data():
-    """asdf"""
+    """Read in a dataframe of FIPS code - Interconnect crosswalk.
+
+        :return:                        Dataframe of each US county's associated electricity interconnection
+
+        """
 
     data = pkg_resources.resource_filename('flow', 'data/county_interconnect_list.xlsx')
 
@@ -25,7 +29,11 @@ def get_interconnect_data():
 
 
 def get_water_use_1995():
-    """asdf"""
+     """Read in a dataframe of 1995 USGS Water Use Data.
+
+        :return:                        Dataframe of a number of water values for 1995 at the county level
+
+        """
 
     data = pkg_resources.resource_filename('flow', 'data/usco1995.xlsx')
 
