335381dec814c3c0378fecfc67713c98ffd7ace6
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2021-11-09 16:41:52 -0800

    add reader tests

--- flow/tests/test_reader.py
+++ flow/tests/test_reader.py
@@ -18,6 +18,29 @@ class TestReader(unittest.TestCase):
         # expect FIPS as a column name
         self.assertTrue('FIPS' in df.columns)
 
+    def test_get_water_use_1995(self):
+        """Ensure we get what is expected from the input file."""
+
+        # load the data
+        df = get_water_use_1995()
+
+        # expect StateCode as a column name
+        self.assertTrue('StateCode' in df.columns)
+
+        # expect CountyCode as a column name
+        self.assertTrue('CountyCode' in df.columns)
+
+    def test_get_interconnect_data(self):
+        """Ensure we get what is expected from the input file."""
+
+        # load the data
+        df = get_interconnect_data()
+
+        # expect FIPS as a column name
+        self.assertTrue('FIPS' in df.columns)
+
+        # expect Interconnect as a column name
+        self.assertTrue('Interconnect' in df.columns)
 
 if __name__ == '__main__':
     unittest.main()
