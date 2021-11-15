22aabb3af4451fd10905ae61cc42ea9932604ca0
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2021-11-15 13:28:00 -0800

    Add prep 1995 data to clean

--- flow/clean.py
+++ flow/clean.py
@@ -32,7 +32,7 @@ def prep_water_use_2015() -> pd.DataFrame:
 
 
 def prep_water_use_1995() -> pd.DataFrame:
-    """prepping USGS 2015 water use data by replacing missing values, fixing FIPS codes,
+    """prepping USGS 1995 water use data by replacing missing values, fixing FIPS codes,
      and reducing to needed variables
 
     :return:                DataFrame of a number of water values for 1995 at the county level
@@ -40,19 +40,42 @@ def prep_water_use_1995() -> pd.DataFrame:
     """
 
     # read in water use data for 2015 in million gallons per day by county
-    df = get_water_use_2015()
+    df = get_water_use_1995()
 
-    # replacing characters for missing USGS data with value of zero
-    df.replace("--", 0, inplace=True)
+    # create a complete FIPS code from the sum of the state and county level FIPS codes
+    df["FIPS"] = df["StateCode"] + df["CountyCode"]
 
-    # creating a list of required variables from full dataset
-    variables_list = ['StateCode', 'CountyCode', 'DO-CUTot', 'DO-WDelv', 'CO-CUTot', 'IN-CUsFr', 'CO-WDelv',
-                      'IN-WFrTo', 'IN-PSDel', 'IN-CUsSa', 'IN-WSaTo', 'MI-CUTot', 'MI-WTotl',
-                      'PT-WSWFr', 'MI-CUsFr', 'MI-WFrTo', 'MI-CUsSa', 'MI-WSaTo', 'LV-CUTot',
-                      'LV-WTotl', 'LA-CUTot', 'LA-WTotl', 'IR-CUTot', 'IR-WTotl', 'HY-InUse',
-                      'HY-InPow', 'IR-CLoss'
+    # creating a list of required variables from full 1995 dataset
+    variables_list = ['FIPS', 'DO-CUTot', 'DO-WDelv', 'CO-CUTot', 'IN-CUsFr',
+                      'CO-WDelv', 'IN-WFrTo', 'IN-PSDel', 'IN-CUsSa', 'IN-WSaTo', 'MI-CUTot',
+                      'MI-WTotl', 'PT-WSWFr', 'MI-CUsFr', 'MI-WFrTo', 'MI-CUsSa', 'MI-WSaTo',
+                      'LV-CUTot', 'LV-WTotl', 'LA-CUTot', 'LA-WTotl', 'IR-CUTot', 'IR-WTotl',
+                      'HY-InUse', 'HY-InPow', 'IR-CLoss'
                       ]
+
     # reducing dataframe to variables in variables_list
     df = df[variables_list]
+
+    # FIPS code changes between 1995 and 2015
+    df['FIPS'] = np.where(df['FIPS'] == "12025", "12086", df['FIPS'])  # Miami-Dade County, FL
+    df['FIPS'] = np.where(df['FIPS'] == "46113", "46102", df['FIPS'])  # Oglala Lakota County, SD
+    df['FIPS'] = np.where(df['FIPS'] == "02232", "02105", df['FIPS'])  # Hoonah-Angoon Census Area, AK
+    df['FIPS'] = np.where(df['FIPS'] == "02270", "02158", df['FIPS'])  # Kusilvak Census Area, AK
+    df['FIPS'] = np.where(df['FIPS'] == "02280", "02195", df['FIPS'])  # Petersburg Borough, AK
+    df['FIPS'] = np.where(df['FIPS'] == "02201", "02198", df['FIPS'])  # Wales-Hyder Census Area, AK
+    df['FIPS'] = np.where(df['FIPS'] == "78001", "78010", df['FIPS'])  # St. Croix County, VI
+    df['FIPS'] = np.where(df['FIPS'] == "78003", "78020", df['FIPS'])  # St. John County, VI
+    df['FIPS'] = np.where(df['FIPS'] == "78004", "78030", df['FIPS'])  # St. Thomas County, VI
+
+    # Copies data from counties that split into multiple FIPS codes between 1995 and 2015 into new rows
+    wrangell_petersburg_index = df.index[df['FIPS'] == "02280"].tolist()  # Wrangell, AK from Wrangell-Petersburg, AK
+    df = df.append(df.loc[wrangell_petersburg_index * 1].assign(FIPS="02275"), ignore_index=True)
+
+    skagway_index = df.index[df['FIPS'] == "02232"].tolist()  # Hoonah-Angoon, AK from Skagway-Hoonah-Angoon, AK
+    df = df.append(df.loc[skagway_index * 1].assign(FIPS="02230"), ignore_index=True)
+
+    boulder_index = df.index[df['FIPS'] == "08013"].tolist()  # Broomfield County, CO from Boulder County, CO
+    df = df.append(df.loc[boulder_index * 1].assign(FIPS="08014"), ignore_index=True)
+
 
     return df
