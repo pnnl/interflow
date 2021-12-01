9bc795234e819c57515e771c4f356c9240f40cf2
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2021-12-01 11:36:17 -0800

    add wastewater reader tests

--- flow/tests/test_clean.py
+++ flow/tests/test_clean.py
@@ -45,6 +45,50 @@ class TestClean(unittest.TestCase):
 
         # no null data rows
         rows_with_null = [index for index, row in df.iterrows() if row.isnull().any()]
+        self.assertEqual(rows_with_null, [])
+
+    def test_prep_wastewater_data(self):
+        """Ensure we get what is expected from the county identifier data."""
+
+        # load data
+        df = get_county_identifier_data()
+
+        # no null data rows
+        rows_with_null = [index for index, row in df.iterrows() if row.isnull().any()]
+        self.assertEqual(rows_with_null, [])
+
+        # ensure FIPS column is type float
+        self.assertEqual(df["FIPS"].dtypes, 'O')
+
+        # load data
+        df = prep_wastewater_data()
+
+        # no null data rows
+        rows_with_null = [index for index, row in df.iterrows() if row.isnull().any()]
+        self.assertEqual(rows_with_null, [])
+
+        # ensure FIPS column is type float
+        self.assertEqual(df["FIPS"].dtypes, 'O')
+
+        # ensure total wastewater is sum of infiltration and non-infiltration
+        self.assertAlmostEqual(df["EXIST_AVG"].sum() + df["EXIST_INFILTRATION"].sum(),
+                               df["EXIST_TOTAL"].sum(),
+                               delta=1)
+
+        # ensure all discharge water is equal to total water processed
+        col_list = ['ww_evap', 'ww_gd', 'ww_in', 'ww_ir', 'ww_od',
+                    'ww_ps', 'ww_sd', 'ww_ww']
+        self.assertAlmostEqual(df[col_list].sum(axis=1).sum(),
+                               df["EXIST_TOTAL"].sum(),
+                               delta=1)
+
+        # ensure all treated water is equal to total water processed
+        col_list = ['no_treatment', 'ww_adv', 'ww_prim', 'ww_sec']
+        self.assertAlmostEqual(df[col_list].sum(axis=1).sum(),
+                               df["EXIST_TOTAL"].sum(),
+                               delta=1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
