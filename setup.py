17f12d17dd8e2282b43b569590edaef65d603c06
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2022-02-10 11:27:16 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import re
 from setuptools import setup, find_packages
 
+
 def readme():
     """Return the contents of the project README file."""
     with open('README.md') as readme:
         return readme.read()
 
+
 # get version from __init__ of package
 version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", open('flow/__init__.py').read(), re.M).group(1)
-
 
 setup(
     name='flow',
@@ -18,7 +19,8 @@ setup(
     license='BSD2-Clause',
     author='Kendall Mongird',
     author_email='kendall.mongird@pnnl.gov',
-    description='An open-source Python package for calculating water and energy interdependencies across sectors',
+    description='An open-source Python package for calculating multi-sectoral '
+                'and multi-unit flows and interdependencies',
     long_description=readme(),
     long_description_content_type="text/markdown",
     python_requires='>=3.7.*, <4',
@@ -27,6 +29,6 @@ setup(
         'pandas>=1.3.4',
         'PyYAML>=5.4.1',
         'matplotlib>=3.3.3'
-        'plotly>=5.0'
+        'plotly>=5.5'
     ]
 )
