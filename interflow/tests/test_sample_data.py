f9b5409c1cbf56f83e62a22308af28fba72a0687
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2022-03-17 10:13:54 -0700

    Create test_sample_data.py

---
+++ interflow/tests/test_sample_data.py
@@ -0,0 +1,15 @@
+import unittest
+from interflow.sample_data import *
+
+
+class MyTestCase(unittest.TestCase):
+    """Conduct tests for functions of sample_data.py."""
+
+    def test_something(self):
+
+        
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
