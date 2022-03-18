6dd607734638cca26f233cc0e8b750f4374d824a
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2022-03-18 10:34:18 -0700

    add test_calc_sc_ww_values

--- interflow/tests/test_sample_data.py
+++ interflow/tests/test_sample_data.py
@@ -480,6 +480,30 @@ class MyTestCase(unittest.TestCase):
         expected_county_county = 3142
         self.assertEqual(output_county_count, expected_county_county)
 
+    def test_calc_sc_ww_values(self):
+
+        # collect output
+        output = calc_sc_ww_values()
+
+        # make sure only south carolina is included
+        output_state = output.groupby("State", as_index=False).sum()
+        self.assertEqual(1, len(output_state))
+
+        # make sure there are no blank values
+        is_nan = output.isnull()
+        row_has_nan = is_nan.any(axis=1)
+        rows_with_nan = output[row_has_nan]
+        result = len(rows_with_nan)
+        self.assertEqual(result, 0)
+
+        # check that dropped variables are not in output
+        drop_list = ['total_municipal', 'advanced_municipal', 'secondary_municipal']
+        check_list = []
+        for item in drop_list:
+            check_list.append(item in output.columns)
+        self.assertFalse(True in check_list)
+
+
 
 
 if __name__ == '__main__':
