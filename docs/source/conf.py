9d100ecb86ad4d602f9e13d18dbb958efac7ded5
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2022-03-24 15:58:35 -0700

    update description in index.html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
-html_theme_options = {'description': 'a multi-resource sectoral interdependency flow builder'}
+html_theme_options = {'description': 'a multi-sectoral resource interdependency flow builder'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
